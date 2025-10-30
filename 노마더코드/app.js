466ac04c690a620966aa36462043e1b52400e003
hyelin99 <wjsgpfls@icloud.com>
2025-10-31 08:40:10 +0900

    2025.10.31

--- 노마더코드/app.js
+++ 노마더코드/app.js
@@ -1,3 +1,20 @@
 console.log("hello")
 console.log("^__^")
 alert("hi")
+
+const a=5
+console.log(a+5)
+
+const amifat=true;
+console.log(amifat)
+
+const numbers=["one","two","three","fore","five","six"]
+console.log(numbers)
+
+numbers.push("sven")
+console.log(numbers)
+
+const name="태연"
+const name2="혜린"
+console.log(name+ name2)
+
