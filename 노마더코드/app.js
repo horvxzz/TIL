7855921464025552f8b93d53abe98dc1ac9c5461
hyelin99 <wjsgpfls@icloud.com>
2025-11-06 20:44:26 +0900

    2025.11.06

--- 노마더코드/app.js
+++ 노마더코드/app.js
@@ -2,19 +2,30 @@ console.log("hello")
 console.log("^__^")
 alert("hi")
 
-const a=5
+const a=5;
 console.log(a+5)
 
 const amifat=true;
 console.log(amifat)
 
-const numbers=["one","two","three","fore","five","six"]
+const numbers=["one","two","three","fore","five","six"];
 console.log(numbers)
 
 numbers.push("sven")
 console.log(numbers)
 
-const name="태연"
-const name2="혜린"
+const name="태연";
+const name2="혜린";
 console.log(name+ name2)
 
+
+const player={
+    name:"혜린",
+    point:121212,
+    fat:"litte bit"
+}
+
+console.log(player)
+console.log(player.name)
+player.lastname="태연"
+console.log(player)
